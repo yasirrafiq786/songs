55843e75160c6a5f26485cd6f421fc60f1c53091
Yasir Malik <yasirrafiq786@gmail.com>
2020-08-06 17:05:56 +1000

    started making reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+const songsReducer = () => {
+  return [
+    {title: 'No Scrubs', duration: '4:05'},
+    {title: 'Macarena', duration: '3:25'},
+    {title: 'All Star', duration: '2:30'},
+    {title: 'I Want it That Way', duration: '1:25'},
+  ];
+};
